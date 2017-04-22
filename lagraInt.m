b34eb164388ac5c6a9389c0a33ec15e5a1b7425a
hematilldyl <d.hematillake@gmail.com>
2017-04-22 17:40:58 -0400

    Update and rename lab3.m to lagraInt.m

---
+++ lagraInt.m
@@ -0,0 +1,35 @@
+function yresult = lagraInt(x)
+%{
+    Author: Dylan Hematillake
+    Date: March 8th, 2017
+    Program Description: Uses the lagrange interpolation
+                         method to interpolate csv data
+%}
+
+
+%measured values
+%xVal = [1 2 3 4 5 6];
+%yVal = [0 6 2 1 5 10];
+
+%change filename depending on location
+filename = 'd:\MatlabPrograms\data.csv';
+delim = ',';
+headerln = 1;
+datapoints = importdata(filename,delim,headerLn);
+[xvals,yvals]=[datapoints.data(:,1),datapoints.data(:,2)]
+n = length(xVal)
+
+sum = 0;
+
+for i = 1:n
+    psum = 1;
+    for j = 1:n
+        if(j ~= i)
+            psum = (x-xVal(j))/(xVal(i)-xVal(j)) *psum;
+        end
+    end
+    sum = (yVal(i)*psum)+sum;
+end
+
+yresult = sum;
+end
