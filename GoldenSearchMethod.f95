acc55b16cb107b1dac5578842d36fbe5645cd529
hematilldyl <d.hematillake@gmail.com>
2018-03-12 11:35:05 -0400

    Update GoldenSearchMethod.f95

--- GoldenSearchMethod.f95
+++ GoldenSearchMethod.f95
@@ -1,25 +1,25 @@
 PROGRAM example
-	
-	IMPLICIT NONE
-    REAL  :: x1,x2,STOPC,GoldenSearchMethod,maxAns
-    x1 = 0
-    x2=1
-    STOPC = 1
+
+    IMPLICIT NONE
+    INTEGER, PARAMETER  :: ikind=selected_real_kind(p=18)
+    REAL (KIND=ikind):: x1,x2,STOPC,GoldenSearchMethod,maxAns
+
+    PRINT*, 'Enter the start and end bounds and stop criteria'
+    READ*, x1,x2,STOPC
     maxAns = GoldenSearchMethod(STOPC,x1,x2)
     PRINT*, "Root is:",maxAns
 
 END PROGRAM example
-    
 
-REAL FUNCTION GoldenSearchMethod(SC,xO,xN)
 
-    REAL ::  d,X1,X2,xopt,GR,xO,xN
-    REAL, INTENT(IN) :: SC
-    
-	REAL, DIMENSION(100) :: ea
+REAL (KIND=selected_real_kind(p=18)) FUNCTION GoldenSearchMethod(SC,xO,xN)
+    INTEGER, PARAMETER  :: ikind=selected_real_kind(p=18)
+    REAL (KIND=ikind)::  d,X1,X2,xopt,GR,xO,xN
+    REAL(KIND=ikind),INTENT(IN) :: SC
+ 	REAL (KIND=ikind), DIMENSION(1000) :: ea
     INTEGER :: numIts
-    
-	ea(1) = 100
+
+   	ea(1) = 100
 	numIts = 1
 	GR = 0.61803
 
@@ -29,7 +29,7 @@ REAL FUNCTION GoldenSearchMethod(SC,xO,xN)
   		X1 = xO +d
   		X2 = xN-d
   	IF(sin(X1)>sin(X2)) THEN
-    	xN=X2
+    	xO=X2
    		xopt = X1
   	ELSE IF(sin(X1)<sin(X2)) THEN
     	xN=X1
@@ -37,10 +37,10 @@ REAL FUNCTION GoldenSearchMethod(SC,xO,xN)
   	END IF
   		ea(numIts)=(1-GR)*abs((xN-xO)/xopt)*100
 	END DO
-
 	GoldenSearchMethod = xopt
+
     RETURN
-    
 END FUNCTION GoldenSearchMethod
+
 
 
